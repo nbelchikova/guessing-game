9301fdcb5570052dfe066fbcdf8ed8412be5dfb9
nbelchikova <n.belchikova@yandex.ru>
2022-08-08 12:59:47 +0600

    docs: change js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.number = Math.round((this.max - this.min) / 2) + this.min;
+      return this.number;
     }
 
     lower() {
-
+      this.max = this.number;
     }
 
     greater() {
-
+      this.min = this.number;
     }
 }
 
